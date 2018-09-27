863e6847ffa6f38f97c9969d43919cbbf72ad4bd
Tanguy Antoine <antoine@bearer.sh>
2018-09-27 13:39:48 +0200

    remove unnecessary component and style feature action

--- NewSlack/views/feature-action.tsx
+++ NewSlack/views/feature-action.tsx
@@ -3,7 +3,7 @@
 
 */
 
-import Bearer, { RootComponent, Prop, Events } from '@bearer/core'
+import Bearer, { RootComponent, Prop, Events, Intent, BearerFetch, State } from '@bearer/core'
 import '@bearer/ui'
 
 @RootComponent({
@@ -11,10 +11,20 @@ import '@bearer/ui'
   group: 'feature'
 })
 export class FeatureAction {
+  @Intent('Share')
+  fetcher: BearerFetch
   @Prop({ mutable: true })
   authIdentifier: string
   @Prop({ mutable: true })
   channelId: string
+  @Prop()
+  text: string = 'Share on slack'
+  @Prop()
+  message: string = 'Eat bananas is super healthy :banana: :arrow_right: :muscle:'
+  @State()
+  loading: boolean = false
+  @State()
+  shared: boolean = false
 
   componentDidLoad() {
     Bearer.emitter.addListener(`bearer:StateSaved:${this.SCENARIO_ID}`, data => {
@@ -25,13 +35,42 @@ export class FeatureAction {
     })
   }
 
+  perform = () => {
+    if (this.shared) {
+      return
+    }
+    this.loading = true
+    this.fetcher({
+      authIdentifier: this.authIdentifier,
+      channelId: this.channelId,
+      body: {
+        message: this.message
+      }
+    })
+      .then(({ data }) => {
+        if (data.ok) {
+          this.shared = true
+        }
+      })
+      .catch(e => {
+        console.error('[BEARER]', 'Error', e)
+      })
+      .then(() => {
+        this.loading = false
+      })
+  }
+
   render() {
     return (
-      <bearer-navigator btnProps={{ content: 'Share', kind: 'primary' }} direction="right">
-        <bearer-navigator-screen name="channel" navigationTitle="Share">
-          <share-slack authIdentifier={this.authIdentifier} channelId={this.channelId} />
-        </bearer-navigator-screen>
-      </bearer-navigator>
+      <bearer-button onClick={this.perform} kind={this.shared ? 'success' : 'primary'}>
+        <span class="text">{this.text}</span>
+        <span class={this.shared ? 'check shared' : 'check'} />
+      </bearer-button>
+      // <bearer-navigator btnProps={{ content: 'Share', kind: 'primary' }} direction="right">
+      //   <bearer-navigator-screen name="channel" navigationTitle="Share">
+      //     <share-slack authIdentifier={this.authIdentifier} channelId={this.channelId} />
+      //   </bearer-navigator-screen>
+      // </bearer-navigator>
     )
   }
 }
