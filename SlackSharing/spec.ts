bf3149cf3cea9644c7576b3a1e707a5f1c1899e1
Radek Molenda <radek.molenda@gmail.com>
2018-10-02 15:26:35 +0200

    feat: update spec.ts to include input and output properties

--- SlackSharing/spec.ts
+++ SlackSharing/spec.ts
@@ -12,28 +12,95 @@ export default {
       isRoot: true,
       initialTagName: 'setup-action',
       group: 'setup',
-      label: 'Setup Action Component'
+      label: 'Setup Action Component',
+      output: {
+        setupId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        }
+      }
     },
     {
       classname: 'ConnectAction',
       isRoot: true,
       initialTagName: 'connect-action',
       group: 'connect',
-      label: 'Connect Action Component'
+      label: 'Connect Action Component',
+      input: {
+        setupId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        }
+
+      },
+      output: {
+        channelId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        }
+      }
     },
     {
       classname: 'ChannelAction',
       isRoot: true,
       initialTagName: 'channel-action',
       group: 'channel',
-      label: 'Channel Action Component'
+      label: 'Channel Action Component',
+      input: {
+        setupId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        }
+
+      },
+      output: {
+        channelId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        }
+      }
     },
     {
       classname: 'FeatureAction',
       isRoot: true,
       initialTagName: 'feature-action',
       group: 'feature',
-      label: 'Feature Action Component'
+      label: 'Feature Action Component',
+      input: {
+        setupId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        },
+        authId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        },
+        channelId: {
+          schema: {
+            type: "string",
+            format: "uuid"
+          }
+        },
+        message: {
+          schema: {
+            type: "string"
+          }
+        }
+      }
     }
   ]
 }
