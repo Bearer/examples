598cba60982ca38689631435c23c62afd0f5ae0f
Tanguy Antoine <antoine@bearer.sh>
2018-10-29 18:53:14 +0100

    fix(slack-slaring): fix authorization rendering
    except if you provide a authId you are considered as not authorized

--- SlackSharing/views/connect-action.tsx
+++ SlackSharing/views/connect-action.tsx
@@ -3,7 +3,7 @@
 
 */
 
-import Bearer, { RootComponent, Events, Event, EventEmitter } from '@bearer/core'
+import Bearer, { RootComponent, Events, Event, EventEmitter, Prop, Element, State } from '@bearer/core'
 import '@bearer/ui'
 import Slack from './components/SlackLogoColor'
 import Cross from './components/IconCross'
@@ -21,10 +21,29 @@ export type TAuthorizedPayload = {
 export class ConnectAction {
   @Event()
   authorized: EventEmitter<TAuthorizedPayload>
+  @Event()
+  revoked: EventEmitter<TAuthorizedPayload>
+
+  @Prop({ mutable: true })
+  authId: string = null
+
+  @State()
+  authIdInternal: string
+  @Element()
+  el: HTMLElement
 
   componentDidLoad() {
+    this.authIdInternal = this.authId
+
     Bearer.emitter.addListener(Events.AUTHORIZED, ({ data }: { data: TAuthSavedPayload }) => {
-      this.authorized.emit({ authId: data.authId })
+      const authId = data.authId || (data as any).authIdentifier
+      this.authId = this.authIdInternal = authId
+      this.authorized.emit({ authId })
+    })
+
+    Bearer.emitter.addListener(Events.REVOKED, (_payload: { data: TAuthSavedPayload }) => {
+      this.authIdInternal = this.authId = null
+      this.revoked.emit()
     })
   }
 
@@ -37,16 +56,28 @@ export class ConnectAction {
     </bearer-button>
   )
 
-  renderAuthorized = ({ revoke }) => (
-    <bearer-button kind="danger" outline onClick={revoke}>
-      <span class="root">
-        <Cross />
-        <span>Revoke access to your Slack</span>
-      </span>
-    </bearer-button>
-  )
+  renderUnauthorizedIfAuthId = ({ authenticate }) => this.authIdInternal && this.renderUnauthorized({ authenticate })
+
+  authenticate = () => {
+    this.el.querySelector('bearer-authorized').authenticate()
+  }
 
   render() {
-    return <bearer-authorized renderUnauthorized={this.renderUnauthorized} renderAuthorized={this.renderAuthorized} />
+    return [
+      <bearer-authorized
+        renderUnauthorized={this.renderUnauthorizedIfAuthId}
+        renderAuthorized={({ revoke }) =>
+          this.authIdInternal && (
+            <bearer-button kind="danger" outline onClick={revoke}>
+              <span class="root">
+                <Cross />
+                <span>Revoke access to your Slack</span>
+              </span>
+            </bearer-button>
+          )
+        }
+      />,
+      !this.authIdInternal && this.renderUnauthorized({ authenticate: this.authenticate })
+    ]
   }
 }
